171a390aee2f06e21fc3c11a870652e054ff7045
fejao123 <143644822+fejao123@users.noreply.github.com>
2025-07-03 11:16:10 -0300

    piroca

--- script.js
+++ script.js
@@ -3,3 +3,80 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas")
 const caixaAlternativas = document.querySelector(".caixa-alternativa")
 const caixaResultado = document.querySelector(".caixa-resultado")
 const textoResultado = document.querySelector(".texto-resultado")
+
+const perguntas=[
+{
+    enunciado:"quem foi o campeão da euro copa de 2024?",
+    alternativas: [
+   {
+    texto:"Espanha",
+    afirmação:"resposta correta",
+    pontos:1
+    },
+    {
+        texto:"França",
+        afirmação:"resposta incoreta",
+        pontos:0
+      }
+    ]
+}, 
+{
+    enunciado:"Quem foi o campeão da nations league?",
+    alternativas: [
+   {
+    texto:"Portugal",
+    afirmação:"resposta correta",
+    pontos:1
+    },
+    {
+        texto:"Croçia",
+        afirmação:"resposta incoreta",
+        pontos:0
+      }
+    ]
+}, 
+{
+    enunciado:"quem foi o campeão da copa do mundo de 2014?",
+    alternativas: [
+   {
+    texto:"Alemanha",
+    afirmação:"resposta correta",
+    pontos:1
+    },
+    {
+        texto:"Argentina",
+        afirmação:"resposta incoreta",
+        pontos:0
+      }
+    ]
+}, 
+{
+    enunciado:"quem foi o campeão da chanpions league de 2025?",
+    alternativas: [
+   {
+    texto:"Psg",
+    afirmação:"resposta correta",
+    pontos:1
+    },
+    {
+        texto:"Inter de Milão",
+        afirmação:"resposta incoreta",
+        pontos:0
+      }
+    ]
+}, 
+{
+    enunciado:"quem foi o campeão da Libertdores 2024?",
+    alternativas: [
+   {
+    texto:"Botafogo",
+    afirmação:"resposta correta",
+    pontos:1
+    },
+    {
+        texto:"Atletico Mineiro",
+        afirmação:"resposta incoreta",
+        pontos:0
+      }
+    ]
+}, 
